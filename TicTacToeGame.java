c434104fc0bf0b25069214337fedfe0d36aec870
shahbaj <shahbaj.49408@gmail.com>
2020-11-10 16:29:39 +0530

    Added: UC1 Initializing Board.

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,14 @@
-public class TicTacToeGame{
-	public static void main(String[] args){
-		System.out.println("Welcome to TicTacToe Game.");
-	}
+public class TicTacToeGame {
+
+   public static char[] initBoard(){
+      char[] board = new char[10];
+      for(int index=1;index<board.length;index++){
+         board[index]=' ';
+      }
+          return board;
+   }
+
+      public static void main(String[] args){
+        TicTacToeGame.initBoard();
+    }
 }

859402243af1773fb14605d4e3e3bb4d7e0bf20d
shahbaj <shahbaj.49408@gmail.com>
2020-11-10 17:56:22 +0530

    Added: UC3 Display Game Board.

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class TicTacToeGame {
 
    public static char[] initBoard(){
@@ -15,29 +14,40 @@ public class TicTacToeGame {
       char player;
       char computer;
 
-      while(True){
+
          System.out.println("Enter player choice 'X' or 'O' ");
-         char value = sc.next().Uppercase().charAt(0);
+         char value = sc.next().toUpperCase().charAt(0);
 
          if(value=='X'){
             player='X';
             computer='O';
-               System.out.println("player choice is:"+player+"computer choice is:"+computer);
-               break;
+               System.out.println("player choice is: " +player+ " computer choice is: " +computer);
+
          }
         else if(value=='O'){
            player='O';
            computer='X';
-                System.out.println("player choice is:"+player+"computer choice is:"+computer);
-                break;
+                System.out.println("player choice is: " +player+ " computer choice is: " +computer);
+
         }
         else{
                 System.out.println("Invalid choice.");
         }
       }
 
+	public static void displayBoard(char[] board){
+	System.out.println("Displaying the Game Board");
+	for(int index1=1;index1<board.length;index1=index1+3){
+		System.out.println("|"+board[index1]+"|"+board[index1+1]+"|"+board[index1+2]);
+		System.out.println("|"+board[index1+3]+"|"+board[index1+4]+"|"+board[index1+5]);
+		System.out.println("|"+board[index1+6]+"|"+board[index1+7]+"|"+board[index1+8]);
+		break;
+		}
+	}
+
    public static void main(String[] args){
         TicTacToeGame.initBoard();
-        TicTakToeGame.playerChoice();
+        TicTacToeGame.playerChoice();
+	TicTacToeGame.displayBoard(TicTacToeGame.initBoard());
     }
 }
